270e816341b7d09854cf418e8a10b6f59a6c3bd8
Diarndt <33708853+Diarndt@users.noreply.github.com>
2018-03-03 17:17:27 -0800

    Lesson-8
    Update to tablebuild.html
    Adding stormcenter.html and thanks.html and related files

---
+++ assignments/lesson-8/scripts/windchill.js
@@ -0,0 +1,14 @@
+   function wind() {
+    var high = parseFloat(document.getElementById("highTemp").innerHTML);
+    var low = parseFloat(document.getElementById("lowTemp").innerHTML);
+  var t = (high + low) / 2;  
+    var s = parseFloat(document.getElementById("windSpeed").innerHTML);
+    var f = windChill(t,s);
+     
+   document.getElementById('windchill').innerHTML=f;
+}
+ function windChill(s,t) {    
+    var chill = (35.74 + 0.6215 * t - 35.75 * Math.pow(s,0.16) + 0.4275 * t * Math.pow(s,0.16));
+   
+    return chill;
+}
